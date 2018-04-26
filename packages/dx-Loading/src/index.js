c4869746c4cb0a5c0458353b78b72fc5a0a7f2ac
linmn <linmn@gddxit.com>
2018-04-26 20:17:11 +0800

    TODO 尝试打包

--- packages/dx-Loading/src/index.js
+++ packages/dx-Loading/src/index.js
@@ -1,45 +1,17 @@
 // loading.js
 import LoadingComponent from './LoadingPage.vue'
 
-let $vm
-
-export default {
-  install(Vue, options = {}) {
-    if (!$vm) {
-      const LoadingPlugin = Vue.extend(LoadingComponent)
-      $vm = new LoadingPlugin({
-        el: document.createElement('div'),
-        data: options
-      })
-      document.body.appendChild($vm.$el)
+// let $vm
+  const dxLoading = {
+    install(Vue, options = {}) {
+      console.info(1)
+      Vue.component('dxLoading', LoadingComponent)
+      console.info(2)
     }
-    $vm.visible = false
-    const loading = {
-      show({ text, durTime, background, color } = options) {
-        $vm.visible = true
-        $vm.text = text
-        $vm.background = background
-        $vm.color = color
-
-        if (durTime) {
-          // 定时关闭
-          setTimeout(() => {
-            $vm.visible = false
-          }, durTime)
-        }
-      },
-      hide() {
-        $vm.visible = false
-      }
-    }
-    if (!Vue.$loading) {
-      Vue.$loading = loading
-    }
-    // Vue.prototype.$loading = Vue.$loading
-    Vue.mixin({
-      created() {
-        this.$loading = Vue.$loading
-      }
-    })
   }
-}
+  // 这里的判断很重要
+  if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(dxLoading)
+  }
+  export default dxLoading
+
